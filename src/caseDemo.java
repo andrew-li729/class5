ef1be4e1e47263c03d89a6bd71564d641b315a4c
Andrew <andrew.li729@gmail.com>
2019-09-17 14:24:41 -0500

    Made string lowercase

--- src/caseDemo.java
+++ src/caseDemo.java
@@ -5,6 +5,9 @@ public class caseDemo {
         String str="HELLO";
         int len=str.length();
         System.out.println("The length of the string is "+len);
-        
+        for(int i=0;i<len;i++){
+            char c=Character.toLowerCase(str.charAt(i));
+            System.out.println(c);
+        }
     }
 }
