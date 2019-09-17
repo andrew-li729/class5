b9310d41ed585352fb58f37d31fc3546d3d5d15c
Andrew <andrew.li729@gmail.com>
2019-09-17 14:15:36 -0500

    We found the length of the string

---
+++ src/caseDemo.java
@@ -0,0 +1,10 @@
+public class caseDemo {
+    public static void main(String[]args){
+        System.out.println("Text processing demo");
+
+        String str="HELLO";
+        int len=str.length();
+        System.out.println("The length of the string is "+len);
+        
+    }
+}
